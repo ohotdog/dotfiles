b2603e830580c9def4af186c38259f9bd94ec4f7
Alec Campbell <thisiswaysilly@gmail.com>
2017-06-20 13:25:10 -0400

    Updating verbiage

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,40 +1,37 @@
 #!/bin/bash
-####################
-# .make.sh
-# Create symlinks from the home directory to dotfiles
-####################
-
+##Create symlinks from the home directory to dotfiles##
+# Set variables
 dir=~/dotfiles
-olddir=~/dotfiles_old
+olddir=~/.dotfiles_old
 files="bg bash_profile bashrc gitconfig inputrc vimrc xinitrc config/i3/config"
 update=false
 
-# check if files exist before continuing
+# Check if files exist before continuing
 for file in $files; do
     if [ ! -h ~/.$file ]; then
        update=true 
     fi
 done
 
-# if no file exists exit
+# If no file exists exit
 if [ $update = false ]; then
     echo "Nothing to udpate"
     exit 1
 fi
 
-# create dotfiles_old in ~
+# Create dotfiles_old in ~
 if [ ! -d "$olddir" ]; then
     echo "Creating $olddir for backup of any existing dotfiles in ~"
     mkdir $olddir
     echo "...done"
 fi
 
-# change to the dotfiles directory
+# Change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in ~ to dotfiles_old dir, then create symlinks
+# Move any existing dotfiles in ~ to dotfiles_old dir, then create symlinks
 for file in $files; do
     if [ ! -h ~/.$file ]; then
         echo "Moving $file from ~ to $olddir"
