2b124d4b080322ef1875bdcf76c91c51f7ad43b7
Alec Campbell <aleccamp94@gmail.com>
2017-06-23 02:17:00 -0400

    When will the madness end. More files added

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -3,7 +3,7 @@
 # Set variables
 dir=~/dotfiles
 olddir=~/.dotfiles_old
-files="bg bash_profile bashrc gitconfig inputrc vimrc xinitrc Xresources config/i3/config config/conky/config config/dunst/dunstrc config/polybar/config config/polybar/launch.sh config/user-dirs.dirs"
+files="bash_profile bashrc gitconfig inputrc vimrc xinitrc Xresources config/i3/config config/dunst/dunstrc config/polybar/config config/polybar/launch.sh config/user-dirs.dirs vim/bundle/Vundle.vim"
 update=false
 
 # Check if files exist before continuing
