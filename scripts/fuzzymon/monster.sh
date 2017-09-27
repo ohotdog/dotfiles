056bf80010f435b3b171c49f2183ee7362f80eb7
Alec Campbell <thisiswaysilly@gmail.com>
2017-09-27 03:23:08 -0400

    monster manual shizz

---
+++ scripts/fuzzymon/monster.sh
@@ -0,0 +1,3 @@
+# Kind of a hacky method of opening
+# a firefox window with the monster manual entry
+firefox "file:///home/alec/barovia/mm.html?monster=`python ~/dotfiles/scripts/fuzzymon/fuzzy.py $1`"
