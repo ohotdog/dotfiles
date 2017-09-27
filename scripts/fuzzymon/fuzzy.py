e33810793aefe5f239ee07bd9de6a112764f74e5
Alec Campbell <aleccamp94@gmail.com>
2017-09-27 03:23:08 -0400

    monster manual shizz

---
+++ scripts/fuzzymon/fuzzy.py
@@ -0,0 +1,20 @@
+# hacky script to fuzzily find a entry from a list
+# of monsters and print found entry
+import re
+import sys
+
+with open('/home/alec/dotfiles/scripts/fuzzymon/monsters.txt') as f:
+    monsters = f.readlines()
+monsters = [x.strip() for x in monsters]
+
+def fuzzyfinder(user_input, collection):
+    suggestions = []
+    pattern = '.*?'.join(user_input)
+    regex = re.compile(pattern)
+    for item in collection:
+        match = regex.search(item)
+        if match:
+            suggestions.append((len(match.group()), match.start(), item))
+    return [x for _, _, x in sorted(suggestions)]
+
+print(fuzzyfinder(sys.argv[1:],monsters)[0])
