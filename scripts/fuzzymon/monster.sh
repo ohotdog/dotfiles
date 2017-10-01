5fc1b1cf123f8ebebe68f2137c6573362bb3c7e4
Alec Campbell <thisiswaysilly@gmail.com>
2017-10-01 03:00:19 -0400

    improved honesty

--- scripts/fuzzymon/monster.sh
+++ scripts/fuzzymon/monster.sh
@@ -1,3 +1,2 @@
-# Kind of a hacky method of opening
-# a firefox window with the monster manual entry
+# Complete hack method of opening a firefox window with the monster manual entry
 firefox "file:///home/alec/barovia/mm.html?monster=`python ~/dotfiles/scripts/fuzzymon/fuzzy.py $1`"
