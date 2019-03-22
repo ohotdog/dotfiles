a2aba359fdbc11fcf66d02a9103abc3ace03ba6b
Alec Campbell <thisiswaysilly@gmail.com>
2019-03-21 22:32:58 -0400

    mpv subs and workflow stuff

--- scripts/fuzzymon/monster.sh
+++ scripts/fuzzymon/monster.sh
@@ -1,2 +1,2 @@
 # Complete hack method of opening a firefox window with the monster manual entry
-firefox "file:///home/alec/barovia/mm.html?monster=`python ~/dotfiles/scripts/fuzzymon/fuzzy.py $1`"
+firefox "file:///home/alec/documents/rpgs/barovia/mm.html?monster=`python ~/dotfiles/scripts/fuzzymon/fuzzy.py $1`"
